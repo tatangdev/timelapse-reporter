2fa61ed3c648a0d9794102c549affc7c8d9c04ef
Tatang Romadhona <romadhonatatang@gmail.com>
2024-11-17 11:33:48 +0700

    feat: migrate from redis to ioredis

--- src/libs/redis.ts
+++ src/libs/redis.ts
@@ -1,7 +1,25 @@
 import Redis from 'ioredis';
 
 const REDIS_URL: string = process.env.REDIS_URL || 'redis://localhost:6379';
-const redisClient = new Redis(REDIS_URL);
+
+const redisClient = new Redis(REDIS_URL, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+    reconnectOnError: (err) => {
+        if (err.message.includes('getaddrinfo ENOTFOUND')) {
+            console.error('Reconnecting due to DNS error:', err);
+            return true;
+        }
+        return false;
+    },
+});
+
+redisClient.on('error', (err) => {
+    console.error('[ioredis] Connection Error:', err);
+});
+
+export default redisClient;
+
 // import { createClient } from 'redis';
 
 // const REDIS_URL: string = process.env.REDIS_URL || 'redis://localhost:6379';
@@ -9,5 +27,3 @@ const redisClient = new Redis(REDIS_URL);
 
 // redisClient.on('error', (err) => console.error('Redis Error:', err));
 // redisClient.connect();
-
-export default redisClient;
